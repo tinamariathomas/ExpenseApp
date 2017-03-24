81c43c893068390b43222aa34c87f97e5f80c179
tinamt <codertina@gmail.com>
2017-03-24 19:34:03 +0530

    Add route for insert expense

--- router/router.go
+++ router/router.go
@@ -1,15 +1,33 @@
 package router
 
 import (
-	"github.com/gorilla/mux"
-
 	h "git.expense-app.com/ExpenseApp/handlers"
+	repo "git.expense-app.com/ExpenseApp/repo"
+
+	_ "github.com/lib/pq"
+	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
+	"fmt"
 )
 
 func GetAPIRoutes() *mux.Router{
 
 	r := mux.NewRouter()
+	expense := &repo.Expense{}
+
+	db := openDBConnection("<username>","<db-name>","<password>")
+
+	insertExpenseHandler := h.AddExpenseHandler(expense, db)
 	r.HandleFunc("/healthcheck", h.HealthCheckHandler).Methods("GET")
+	r.HandleFunc("/expense", insertExpenseHandler).Methods("POST")
 
 	return r
+}
+
+func openDBConnection(username string, dbName string, password string) *sqlx.DB{
+	db, err := sqlx.Connect("postgres", fmt.Sprintf("user=%s dbname=%s password=%s sslmode=disable", username, dbName, password))
+	if err != nil{
+		panic(err)
+	}
+	return db
 }
