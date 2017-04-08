b5757e7d5a70ded41bcd18b2a060eee59a7e7131
tinamt <codertina@gmail.com>
2017-04-08 12:33:41 +0530

    package.json and webpack configurations

--- javascript/src/main.js
+++ javascript/src/main.js
@@ -1,1 +1,11 @@
-console.log('Hello from Webpack');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends React.Component {
+  render () {
+    return <p> But so is React!</p>;
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,30 @@
+var webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+var APP_DIR = path.resolve(__dirname, '/javascript/src');
+var OUTPUT_DIR = path.resolve(__dirname, 'out/public');
+
 module.exports = {
   entry: './javascript/src/main.js',
   output: {
-    filename: './out/public/bundle.js'
-  }
+    path: OUTPUT_DIR,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module : {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.ejs',
+      inject: 'body',
+    })
+  ]
 };
